2cddb2ce2cfad378e2229648419716b456b86ff3
nimbus <chad.wyszynski@gmail.com>
2012-11-03 14:08:38 -0700

    Created DecryptionMapper

---
+++ src/cpsc551/HadoopEncrypt/MapReduce/DecryptionMapper.java
@@ -0,0 +1,49 @@
+/**
+ * Chad Wyszynski for CPSC 551
+ */
+package cpsc551.HadoopEncrypt.MapReduce;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import cpsc551.HadoopEncrypt.Encrypter.Encrypter;
+
+/**
+ * Descrypts data 
+ * @author Chad Wyszynski
+ *
+ */
+public class DecryptionMapper 
+	extends Mapper<IntWritable, BytesWritable,	IntWritable, BytesWritable> 
+{
+	//TODO give this guy the encryption function and block size, setEncrypter?
+	
+	private Encrypter encrypter;
+	
+	public DecryptionMapper() throws NoSuchAlgorithmException
+	{
+		//TODO save/reuse key
+		encrypter = new Encrypter(KeyGenerator.getInstance("AES").generateKey()); 
+	}
+	
+	/**
+	 * Generates (block number, encrypted block) key-value pairs
+	 * @param key block number
+	 * @param value byte string to be encrypted
+	 * @param context to write new key values to;
+	 */
+	public void map(IntWritable key, BytesWritable value, Context context)
+			throws IOException, InterruptedException
+	{
+		context.write(
+				key, 
+				new BytesWritable(encrypter.decrypt(value.getBytes()))
+		);
+	}
+}
