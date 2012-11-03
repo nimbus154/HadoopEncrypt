185eea82774e34fe2ffee5c81c95f0d7d1237b67
nimbus <chad.wyszynski@gmail.com>
2012-11-03 14:07:49 -0700

    Test actual encryption

--- src/cpsc551/HadoopEncrypt/MapReduce/Test/EncryptionMapperTest.java
+++ src/cpsc551/HadoopEncrypt/MapReduce/Test/EncryptionMapperTest.java
@@ -5,12 +5,16 @@ package cpsc551.HadoopEncrypt.MapReduce.Test;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.util.List;
+
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.types.Pair;
 
 import cpsc551.HadoopEncrypt.MapReduce.EncryptionMapper;
 
@@ -32,13 +36,17 @@ public class EncryptionMapperTest {
 	 * Test method for {@link cpsc551.HadoopEncrypt.MapReduce.EncryptionMapper#map(org.apache.hadoop.io.IntWritable, org.apache.hadoop.io.BytesWritable, org.apache.hadoop.mapreduce.Mapper.Context)}.
 	 */
 	@Test
-	public void testMapIntWritableBytesWritableContext() {
-		byte[] testIn = {0, 1, 2, 3, 4, 5, 6, 7};
-		byte[] exOut = {1, 2, 3, 4, 5, 6, 7, 8};
-		new MapDriver<IntWritable, BytesWritable, IntWritable, BytesWritable>()
-			.withMapper(new EncryptionMapper())
-			.withInput(new IntWritable(1), new BytesWritable(testIn))
-			.withOutput(new IntWritable(1), new BytesWritable(exOut))
-			.runTest();			
+	public void testMapIntWritableBytesWritableContext() throws Exception {
+		byte[] input = {0, 1, 2, 3, 4, 5, 6, 7};		
+		List<Pair<IntWritable, BytesWritable>> result = 
+				new MapDriver<IntWritable, BytesWritable, IntWritable, BytesWritable>()
+					.withMapper(new EncryptionMapper())
+					.withInput(new IntWritable(1), new BytesWritable(input))
+					.run();
+		byte[] encrypted = result.get(0).getSecond().getBytes();
+		for(int i = 0; i < input.length; i++)
+			assertThat(input[i], IsNot.not(IsEqual.equalTo(encrypted[i])));
+		
+		
 	}
 }
