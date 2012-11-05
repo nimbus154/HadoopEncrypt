31c6007a1b74963e179341444c9f99cfd7f3e872
nimbus <chad.wyszynski@gmail.com>
2012-11-05 01:08:27 -0800

    Updated to use Text instead of BytesWritable

--- src/cpsc551/HadoopEncrypt/MapReduce/Test/EncryptionMapperTest.java
+++ src/cpsc551/HadoopEncrypt/MapReduce/Test/EncryptionMapperTest.java
@@ -12,14 +12,10 @@ import org.junit.Test;
 
 import java.util.List;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.apache.hadoop.mrunit.types.Pair;
 
-import cpsc551.HadoopEncrypt.Encrypter.Encrypter;
 import cpsc551.HadoopEncrypt.MapReduce.EncryptionMapper;
 
 /**
@@ -29,14 +25,11 @@ import cpsc551.HadoopEncrypt.MapReduce.EncryptionMapper;
  */
 public class EncryptionMapperTest {
 
-	private SecretKey key;
-	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		key = KeyGenerator.getInstance("AES").generateKey();
 	}
 
 	/**
@@ -44,21 +37,18 @@ public class EncryptionMapperTest {
 	 */
 	@Test
 	public void testMapIntWritableBytesWritableContext() throws Exception {
-		Encrypter e = new Encrypter(key);
-		byte[] plaintext = {0, 1, 2, 3, 4, 5, 6, 7};	
-		BytesWritable input = new BytesWritable(plaintext);
-		List<Pair<IntWritable, BytesWritable>> result = 
-				new MapDriver<IntWritable, BytesWritable, IntWritable, BytesWritable>()
-					.withMapper(new EncryptionMapper(e))
-					.withInput(new IntWritable(1), input)
+		String plaintext = "123456";
+		
+		List<Pair<LongWritable, Text>> result = 
+				new MapDriver<LongWritable, Text, LongWritable, Text>()
+					.withMapper(new EncryptionMapper())
+					.withInput(new LongWritable(1), new Text(plaintext))
 					.run();
-		byte[] encrypted = result.get(0).getSecond().getBytes();
-		for(int i = 0; i < plaintext.length; i++)
-			assertThat(plaintext[i], IsNot.not(IsEqual.equalTo(encrypted[i])));
+		String encrypted = result.get(0).getSecond().toString();
+		assertThat(plaintext, IsNot.not(IsEqual.equalTo(encrypted)));
 		
-		
-		byte[] expected = e.encrypt(input.getBytes());		
-		assertEquals(expected.length, encrypted.length);
+		//byte[] expected = e.encrypt(plainBytes);		
+		//assertEquals(expected.length, encrypted.length);
 		
 	}
 }
