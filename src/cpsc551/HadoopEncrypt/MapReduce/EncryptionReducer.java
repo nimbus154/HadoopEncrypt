92aa537a5b5305e34339724f81861aaf3412fa1b
nimbus <chad.wyszynski@gmail.com>
2012-11-04 23:43:41 -0800

    Updated to match with EncryptionMapper

--- src/cpsc551/HadoopEncrypt/MapReduce/EncryptionReducer.java
+++ src/cpsc551/HadoopEncrypt/MapReduce/EncryptionReducer.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 /**
@@ -15,17 +16,21 @@ import org.apache.hadoop.mapreduce.Reducer;
  *
  */
 public class EncryptionReducer extends
-		Reducer<IntWritable, BytesWritable,	IntWritable, BytesWritable> 
+		Reducer<LongWritable, BytesWritable, LongWritable, BytesWritable> 
 {
+	public EncryptionReducer() { } ;
+	
 	/**
 	 * Generates a file of encrypted bytes
 	 * @param key block number
 	 * @param value encrypted block-sized bytes
 	 * @param context to write new key values to;
 	 */
-	public void reduce(IntWritable key, BytesWritable value, Context context)
+	@Override
+	public void reduce(LongWritable key, Iterable<BytesWritable> value, Context context)
 			throws IOException, InterruptedException
 	{
-		context.write(key, value);
+		for(BytesWritable bytes : value)
+			context.write(key, bytes);		
 	}
 }
