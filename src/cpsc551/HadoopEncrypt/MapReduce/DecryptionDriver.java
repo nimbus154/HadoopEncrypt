9c86ebbb8fe8107969d8b9d618d0a82c52ba633f
nimbus <chad.wyszynski@gmail.com>
2012-11-25 18:15:56 -0800

    Changed output separator to space (least obtrusive)

--- src/cpsc551/HadoopEncrypt/MapReduce/DecryptionDriver.java
+++ src/cpsc551/HadoopEncrypt/MapReduce/DecryptionDriver.java
@@ -14,7 +14,7 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 /**
- * Runs the Hadoop encryption MapReduce job
+ * Runs the Hadoop decryption MapReduce job
  * @author Chad Wyszynski
  *
  */
@@ -31,44 +31,49 @@ public class DecryptionDriver extends Configured implements Tool {
 	      ToolRunner.printGenericCommandUsage(System.err);
 	}
 
-	  @Override
-	  public int run(String[] args) throws Exception 
-	  {
+	/**
+	 * Configures and runs the MapReduce job
+	 */
+	@Override
+	public int run(String[] args) throws Exception 
+	{
 		//need encryption key, input, and output files
-	    if (args.length != 3) 
-	    {
-	    	printUsage();
-	      return -1;
-	    }
-	    
-	    //save encryption key so it can be read by mapper
-	    Configuration conf = new Configuration();
-	    conf.set("encryptionKey", args[0]);
-	    conf.set("key.value.separator.in.input.line", "");
-	    
-	    //create a new Hadoop job, set all options
-	    Job job = new Job(conf, "HadoopDecrypt");
-	    job.setJarByClass(getClass());
-
-	    FileInputFormat.addInputPath(job, new Path(args[1]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[2]));
-	    job.setMapperClass(DecryptionMapper.class);
-	    job.setCombinerClass(DecryptionReducer.class);
-	    job.setReducerClass(DecryptionReducer.class);
-	    
-	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(Text.class);
-	    
-	    return job.waitForCompletion(true) ? 0 : 1;
-	  }
-	  
-	  /**
-	   * Main entry point
-	   * @param args
-	   * @throws Exception
-	   */
-	  public static void main(String[] args) throws Exception {
-	    int exitCode = ToolRunner.run(new DecryptionDriver(), args);
-	    System.exit(exitCode);
-	  }
+		if (args.length != 3) 
+		{
+			printUsage();
+		  return -1;
+		}
+		
+		Configuration conf = new Configuration();
+		//save encryption key so it can be read by mapper
+		conf.set("encryptionKey", args[0]);
+		//smallest separator possible b/c not writing keys (blank doesn't work)
+		conf.set("mapred.textoutputformat.separator", "" ); 
+		
+		//create a new Hadoop job, set all options
+		Job job = new Job(conf, "HadoopDecrypt");
+		job.setJarByClass(getClass());
+		    
+		FileInputFormat.addInputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
+		job.setMapperClass(DecryptionMapper.class);
+		job.setCombinerClass(DecryptionReducer.class);
+		job.setReducerClass(DecryptionReducer.class);
+		    
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		    
+		return job.waitForCompletion(true) ? 0 : 1;
 	}
+	  
+	/**
+	* Main entry point
+	* @param args
+	* @throws Exception
+	*/
+	public static void main(String[] args) throws Exception 
+	{
+		int exitCode = ToolRunner.run(new DecryptionDriver(), args);
+		System.exit(exitCode);
+	}
+}
